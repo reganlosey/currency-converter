b7fd34ad121e19bb633cbe095320cdb88723ee73
Regan Losey <regan.losey@gmail.com>
2022-05-30 13:30:24 -0700

    Adjust styling of form and form container

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,16 +1,23 @@
 import './Form.scss';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 const Form: FC = () => {
+  const [source, setSource] = useState<string>('');
+  const [destination, setDestination] = useState<string>('');
+  const [converted, setConverted] = useState<number>(0);
+
+
   return (
     <div className="exchange-form-wrapper">
-      <form className="exhange-form">
-        <input className="form-input form-input--from">
+      <form className="exchange-form">
+        <h2 className="form-text--to">Enter the source currency</h2>
+        <input className="form-input form-input--from" placeholder="(ex: USD)">
         </input>
-        <input className="form-input form-input--to">
+        <h2 className="form-text--from">Rad, and the destination currency</h2>
+        <input className="form-input form-input--to" placeholder="(ex: GBP)">
         </input>
+        <button className="convert-btn">Convert</button>
       </form>
-      <button className="convert-btn">Convert</button>
     </div>
 
   )
