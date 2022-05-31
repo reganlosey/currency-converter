6dfb30e6184cefc48255cb85ae9dda117be26580
Regan Losey <regan.losey@gmail.com>
2022-05-30 20:46:33 -0700

    Format base and destination currencies

--- src/ExchangeCard/ExchangeCard.tsx
+++ src/ExchangeCard/ExchangeCard.tsx
@@ -10,8 +10,6 @@ interface Props {
 const ExchangeCard = ({ conversionInfo }: Props) => {
   let countryCode = Object.keys(conversionInfo.rates)[0]
   const { currency_name, rate, rate_for_amount } = conversionInfo.rates[countryCode]
-  // console.log(currency_name, rate, rate_for_amount)
-  // const [ratesInfo, setRatesInfo] = useState<object>();
   const [baseAmount, setBaseAmount] = useState<string>('');
   const [baseCurrencyName, setBaseCurrencyName] = useState<string>('');
   const [destCurrencyName, setDestCurrencyName] = useState<string>('');
@@ -28,14 +26,16 @@ const ExchangeCard = ({ conversionInfo }: Props) => {
 
 
 
+
   return (
     <div className="exchange-card">
       <div className="conversion-info">
-        <h2>Base amount in {conversionInfo.base_currency_code} is equivalent to converted amount in toType</h2>
-        <h2>The conversion rate from fromType to toType is conversion rate</h2>
-      </div>
-
+        {/* parseFloat("123.456").toFixed(2); */}
+        <h2>{parseFloat(baseAmount).toFixed(2)} in <span className="converted-currency">{baseCurrencyName}</span> is equivalent to {parseFloat(convertedAmount).toFixed(2)} in <span className="converted-currency">{destCurrencyName}</span></h2>
+        <h2>The conversion rate from <span style={{ textTransform: "capitalize"}}>{baseCurrencyName}</span> to <span className="converted-currency">{destCurrencyName}</span> is {conversionRate}</h2>
     </div>
+
+    </div >
 
   )
 
